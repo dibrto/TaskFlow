592945e41bea840f4b25277a45b61ed931ab0ea6
dibr <daniel.bratov@unicontsoft.com>
2026-03-26 15:02:59 +0200

    add main layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from "@angular/router";
-import { Dashboard } from "./features/dashboard/dashboard";
+
 import { Login } from "./features/auth/login/login";
 import { Register } from "./features/auth/register/register";
 
+import { MainLayout } from "./layout/main-layout/main-layout";
+import { Dashboard } from "./features/dashboard/dashboard";
+
 export const routes: Routes = [
-    { path: "dashboard", component: Dashboard },
+    {
+        path: "",
+        component: MainLayout,
+        children: [{ path: "dashboard", component: Dashboard }],
+    },
+
     { path: "login", component: Login },
     { path: "register", component: Register },
 ];
