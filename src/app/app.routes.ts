8874da7dc371a235b0a431510932580d26339e3e
dibr <daniel.bratov@unicontsoft.com>
2026-04-06 17:58:23 +0300

    add route guard for auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from "@angular/router";
 import { LoginComponent } from "@features/auth/login/login.component";
 import { RegisterComponent } from "@features/auth/register/register.component";
 
+import { authGuard } from "@guards/auth-guard/auth-guard";
 import { MainLayoutComponent } from "@layout/main-layout/main-layout.component";
 import { DashboardComponent } from "@features/dashboard/dashboard.component";
 import { BoardComponent } from "@features/board/board.component";
@@ -12,8 +13,8 @@ export const routes: Routes = [
         path: "",
         component: MainLayoutComponent,
         children: [
-            { path: "dashboard", component: DashboardComponent },
-            { path: "board", component: BoardComponent },
+            { path: "dashboard", component: DashboardComponent, canActivate: [authGuard] },
+            { path: "board", component: BoardComponent, canActivate: [authGuard] },
         ],
     },
 
