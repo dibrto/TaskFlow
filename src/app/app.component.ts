e25a2b5ad0acba836eb01294a7d7ba3a9da3d1e8
dibr <daniel.bratov@unicontsoft.com>
2026-04-06 17:39:27 +0300

    use ls user info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { AuthService } from "./core/services/auth/auth.service";
 
 @Component({
     selector: "app-root",
@@ -8,5 +9,5 @@ import { RouterOutlet } from "@angular/router";
     styleUrl: "./app.component.css",
 })
 export class AppComponent {
-    protected readonly title = signal("taskflow");
+    private authService = inject(AuthService);
 }
--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
-import { inject, Injectable } from "@angular/core";
+import { computed, inject, Injectable, signal } from "@angular/core";
 import { UserLogin, UserRegister } from "@interfaces/user";
+import { User } from "@supabase/supabase-js";
 import { ApiService } from "@services/api/api.service";
 import { SupabaseService } from "@services/supabase/supabase.service";
 
@@ -9,6 +10,24 @@ import { SupabaseService } from "@services/supabase/supabase.service";
 export class AuthService {
     private supabase = inject(SupabaseService);
     private api = inject(ApiService);
+
+    user = signal<User | null>(null);
+    isAuthenticated = computed(() => !!this.user());
+
+    constructor() {
+        // sync login/logout
+        this.supabase.client.auth.onAuthStateChange((event, session) => {
+            this.user.set(session?.user ?? null);
+        });
+
+        // after refresh
+        this.loadInitialUser();
+    }
+
+    private async loadInitialUser() {
+        const { data } = await this.supabase.client.auth.getSession();
+        this.user.set(data.session?.user ?? null);
+    }
 
     register({ email, password, username }: UserRegister) {
         const req = {
