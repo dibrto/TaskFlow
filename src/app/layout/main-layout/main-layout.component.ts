d7aed9c07a37eef34442c8bd2e7529499717b73f
dibr <daniel.bratov@unicontsoft.com>
2026-04-15 17:19:15 +0300

    add arrow to sidebar section title logout btn animation

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,12 +1,13 @@
 import { AsyncPipe } from "@angular/common";
 import { Component, inject, OnInit } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
 import { Router, RouterOutlet, RouterLinkWithHref, RouterLinkActive } from "@angular/router";
 import { AuthService } from "@services/auth/auth.service";
 import { BoardService } from "@services/board/board.service";
 
 @Component({
     selector: "app-main-layout",
-    imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive, AsyncPipe],
+    imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive, AsyncPipe, MatIconModule],
     templateUrl: "./main-layout.component.html",
     styleUrl: "./main-layout.component.css"
 })
