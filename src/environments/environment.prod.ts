a8c01feced8e738dc180a5b9875b21e2c83309fb
dibr <daniel.bratov@unicontsoft.com>
2026-04-16 14:16:28 +0300

    add env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     supabaseUrl: "https://jhtvenycradgmzqupdjr.supabase.co",
     supabaseKey: "sb_publishable_hM-f4Q8TZBoYA9y0NRyAvA_9ssmb7Tm"
 };
