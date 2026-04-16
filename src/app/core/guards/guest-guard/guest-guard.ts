74d2b7de752055bda49e033439f55a5f7d4e14fa
dibr <daniel.bratov@unicontsoft.com>
2026-04-16 15:54:55 +0300

    ref guard to use createUrlTree

--- src/app/core/guards/guest-guard/guest-guard.ts
+++ src/app/core/guards/guest-guard/guest-guard.ts
@@ -7,8 +7,7 @@ export const guestGuard: CanActivateFn = (route, state) => {
     const router = inject(Router);
 
     if (authService.isAuthenticated()) {
-        router.navigate(["/dashboard"]);
-        return false;
+        router.createUrlTree(["/dashboard"]);
     }
 
     return true;
