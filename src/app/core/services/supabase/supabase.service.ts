9edf16bdb937baeecc6ab307eedbb1cf8bc5611a
dibr <daniel.bratov@unicontsoft.com>
2026-04-16 13:41:11 +0300

    fox tsconfig rpoblem

--- src/app/core/services/supabase/supabase.service.ts
+++ src/app/core/services/supabase/supabase.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
+import { environment } from "@environments/environment";
 import { createClient } from "@supabase/supabase-js";
-import { environment } from "src/environments/environment";
 
 @Injectable({
-    providedIn: "root",
+    providedIn: "root"
 })
 export class SupabaseService {
     public client = createClient(environment.supabaseUrl, environment.supabaseKey);
