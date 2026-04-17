637bece87036fbd42eacee22f4068db2016d2c07
dibr <daniel.bratov@unicontsoft.com>
2026-04-17 12:49:39 +0300

    add board exists guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,23 +7,25 @@ import { RegisterComponent } from "@features/auth/register/register.component";
 import { authGuard } from "@guards/auth-guard/auth-guard";
 import { MainLayoutComponent } from "@layout/main-layout/main-layout.component";
 import { DashboardComponent } from "@features/dashboard/dashboard.component";
+import { HomeComponent } from "@features/home/home.component";
 import { boardIdGuard } from "@guards/board-id/board-id-guard";
+import { boardExistsGuard } from "@guards/board-exists/board-exists-guard";
 import { BoardComponent } from "@features/board/board.component";
 
 export const routes: Routes = [
-    { path: "", redirectTo: "/login", pathMatch: "full" },
+    { path: "", component: HomeComponent, pathMatch: "full" },
 
     {
         path: "",
         component: MainLayoutComponent,
         children: [
             { path: "dashboard", component: DashboardComponent, canActivate: [authGuard] },
-            { path: "board/:id", component: BoardComponent, canActivate: [authGuard, boardIdGuard] }
+            { path: "board/:id", component: BoardComponent, canActivate: [authGuard, boardIdGuard, boardExistsGuard] }
         ]
     },
 
     { path: "login", component: LoginComponent, canActivate: [guestGuard] },
     { path: "register", component: RegisterComponent, canActivate: [guestGuard] },
 
-    { path: "**", redirectTo: "/login" }
+    { path: "**", redirectTo: "/" }
 ];
