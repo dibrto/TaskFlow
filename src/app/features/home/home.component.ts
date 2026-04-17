37a8d4ca4b4d817282e952715be76f00d4337011
dibr <daniel.bratov@unicontsoft.com>
2026-04-17 16:30:25 +0300

    chngae board task col UI UX

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTooltip } from "@angular/material/tooltip";
+import { CdkDragDrop, DragDropModule, moveItemInArray } from "@angular/cdk/drag-drop";
+import { MatMenuModule } from "@angular/material/menu";
 
 @Component({
     selector: "app-home",
     standalone: true,
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, MatIconModule, MatTooltip, DragDropModule, MatMenuModule],
     templateUrl: "./home.component.html",
     styleUrls: ["./home.component.css"]
 })
-export class HomeComponent {}
+export class HomeComponent {
+    tasks = [
+        { id: 1, title: "Design landing page", description: false },
+        { id: 1, title: "Prepare backlog", description: true },
+        { id: 1, title: "Invite team", description: false }
+    ];
+
+    drop(event: CdkDragDrop<any[]>) {
+        moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
+    }
+}
